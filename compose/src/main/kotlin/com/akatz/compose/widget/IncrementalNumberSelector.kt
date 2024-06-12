aa2cf746ed9ac7cabb1f55951cfb83acdd83322c
ajkatz <ajkatz09@gmail.com>
2024-06-11 23:52:12 -0700

    Improve look on IncrementalNumberSelector

--- compose/src/main/kotlin/com/akatz/compose/widget/IncrementalNumberSelector.kt
+++ compose/src/main/kotlin/com/akatz/compose/widget/IncrementalNumberSelector.kt
@@ -1,20 +1,28 @@
 package com.akatz.compose.widget
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.akatz.colors.background_grey
 
 @Composable
 fun IncrementalNumberSelector(
@@ -25,29 +33,44 @@ fun IncrementalNumberSelector(
 ) {
     Box(
         modifier = Modifier
-            .width(150.dp)
+            .width(130.dp)
             .height(60.dp)
+            .border(2.dp, Color.Black, shape = RoundedCornerShape(8.dp))
     ) {
         Column(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize(),
         ) {
             Text(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth().padding(4.dp),
                 text = title,
                 textAlign = TextAlign.Center
             )
             Row(
                 modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Button(modifier = Modifier.padding(2.dp), onClick = decrementAction) {
+                Button(
+                    modifier = Modifier.scale(1f),
+                    colors = ButtonColors(
+                        containerColor = background_grey,
+                        contentColor = Color.Black,
+                        disabledContainerColor = background_grey,
+                        disabledContentColor = Color.Black
+                    ),
+                    onClick = decrementAction) {
                     Text(text = "<")
                 }
                 Text(
-                    modifier = Modifier.padding(4.dp),
                     text = currentValue.toString()
                 )
-                Button(modifier = Modifier.padding(2.dp), onClick = incrementAction) {
+                Button(modifier = Modifier.scale(1f),
+                    colors = ButtonColors(
+                        containerColor = background_grey,
+                        contentColor = Color.Black,
+                        disabledContainerColor = background_grey,
+                        disabledContentColor = Color.Black
+                    ), onClick = incrementAction) {
                     Text(text = ">")
                 }
             }
--- testenv/src/main/kotlin/com/akatz/testenv/MainActivity.kt
+++ testenv/src/main/kotlin/com/akatz/testenv/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.akatz.testenv
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -35,7 +38,11 @@ class MainActivity : BaseComposeActivity<GradientViewModel>(
         val steps = viewModel?.steps?.collectAsState()?.value ?: 5
         val dpSize = viewModel?.dpSize?.collectAsState()?.value ?: 25
 
-        Row {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
             IncrementalNumberSelector(
                 title = "# of Steps",
                 currentValue = steps,
